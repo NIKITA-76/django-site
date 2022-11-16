ba2c9b933a66a463d763e61bab3e77c45bd35f69
N76 <NIKITA-76>
2022-11-16 14:44:20 +0300

    Try fix smth

--- DBLOG/DBLOG/settings.py
+++ DBLOG/DBLOG/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'DBLOG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['temes'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
