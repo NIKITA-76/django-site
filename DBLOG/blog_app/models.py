34dc1ec9856a424ae811fb4be082372b178b5f3c
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-17 19:41:23 +0300

    Modificate models

--- DBLOG/blog_app/models.py
+++ DBLOG/blog_app/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class ModelPost(models.Model):
-    title = models.CharField
+    author = models.CharField('Автор записи', max_length=100, null="dsf")
+    title = models.CharField('Заголовок записи', max_length=500, null="dsf")
 
     def __str__(self):
         return self.title
