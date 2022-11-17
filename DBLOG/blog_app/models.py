d23b2474093b4c82eb20365a3e0c573be7b4b414
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-17 19:31:59 +0300

    Uncoment new model

--- DBLOG/blog_app/models.py
+++ DBLOG/blog_app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-#class ModelPost(models.Model):
-#    title = models.CharField
-#
-#    def __str__(self):
-#        return self.title
+class ModelPost(models.Model):
+    title = models.CharField
+
+    def __str__(self):
+        return self.title
