d2b3dfd96d21dbb2aa7e53b64342b40a781666fd
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-18 18:24:18 +0300

    Add temp view for check smth

--- DBLOG/blog_app/views.py
+++ DBLOG/blog_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def first_page(request):
     return render(request, 'first_page.html')
+
+
+def mess_page(request):
+    return render(request, 'messenger_page.html')
