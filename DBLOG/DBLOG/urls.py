a7da00f0d2b9f25da7a7d55256fa22f4afe6ab1b
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-19 14:39:52 +0300

    Remake for application Independence

--- DBLOG/DBLOG/urls.py
+++ DBLOG/DBLOG/urls.py
@@ -14,17 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from blog_app import views
 
 
 
 
 urlpatterns = [
-    path('', views.first_page),
+    path('', include('blog_app.urls')),
     path('admin/', admin.site.urls),
-    path('messenger/', views.mess_page),
-    path('tg_bot/', views.tgBot_page),
-    path('kv_app/', views.KivyApp_page),
+
 ]
 
