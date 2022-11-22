e339f000a6ea35dc0e74814eaa21766dde088295
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-22 16:28:27 +0300

    some change in model

--- DBLOG/blog_app/models.py
+++ DBLOG/blog_app/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class ModelPost(models.Model):
     title = models.CharField('Заголовок записи', max_length=100, null=False)
     description = models.TextField(null=True)
+    link = models.TextField(null=True)
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
     time_create = models.DateTimeField(auto_now=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
