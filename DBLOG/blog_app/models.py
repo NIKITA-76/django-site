8e2f6979c3a1220dd7bc9fa9086a227bc91725f2
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-22 15:33:20 +0300

    For view main html

--- DBLOG/blog_app/models.py
+++ DBLOG/blog_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class ModelPost(models.Model):
     title = models.CharField('Заголовок записи', max_length=100, null=False)
-    content = models.TextField(null=True)
+    description = models.TextField(null=True)
     photo = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
     time_create = models.DateTimeField(auto_now=True)
     time_update = models.DateTimeField(auto_now=True)
