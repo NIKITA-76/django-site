15cef5b641a45dfaec2cfd0a94d1c45bce0a1076
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-26 01:26:20 +0300

    Add verbose_name in model

--- DBLOG/blog_app/models.py
+++ DBLOG/blog_app/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 
 class ModelPost(models.Model):
-    title = models.CharField('Заголовок записи', max_length=100, null=False)
-    description = models.TextField(null=True)
-    link = models.TextField(null=True)
-    photo = models.ImageField(upload_to='photo/%Y/%m/%d/', blank=True)
-    time_create = models.DateTimeField(auto_now=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(verbose_name='TITLE', max_length=100, null=False)
+    description = models.TextField(null=True, verbose_name='DESCRIPTION', )
+    link = models.TextField(null=True, verbose_name='LINK', )
+    photo = models.ImageField(upload_to='photo/%Y/%m/%d/', verbose_name='PHOTO', blank=True)
+    time_create = models.DateTimeField(auto_now=True, verbose_name='TIME CREATE', )
+    time_update = models.DateTimeField(auto_now=True, verbose_name='TIME UPDATE', )
+    is_published = models.BooleanField(default=False, verbose_name='IS PUBL', )
 
     def __str__(self):
         return self.title
