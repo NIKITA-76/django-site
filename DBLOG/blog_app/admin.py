4bd99862aea6ba45e50381e75014d0be559accf4
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-26 01:25:58 +0300

    Change admin panel (designation of columns in records)

--- DBLOG/blog_app/admin.py
+++ DBLOG/blog_app/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 from .models import ModelPost
 
 
-admin.site.register(ModelPost)
+class ModelPostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published',)
+    list_display_links = ('id', 'title',)
+    search_fields = ('title', 'content',)
+    list_editable = ('is_published',)
+    list_filter = ('is_published',)
+
+
+admin.site.register(ModelPost, ModelPostAdmin)
