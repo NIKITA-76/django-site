a69a6459c9bbe3c52c5a4fefc57c2d18008c1f19
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-27 18:36:36 +0300

    Add name for url

--- DBLOG/blog_app/urls.py
+++ DBLOG/blog_app/urls.py
@@ -3,6 +3,6 @@ from blog_app import views
 
 urlpatterns = [
     path('', views.first_page),
-    path('contact_to_mail/', views.contact),
+    path('contact_to_mail/', views.contact, name="contact"),
 
 ]
