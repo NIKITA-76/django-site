2c2bfa6576daa6a167093351d55995fd85eeba5f
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-28 00:53:53 +0300

    Fix

--- DBLOG/blog_app/forms.py
+++ DBLOG/blog_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class SendToMail(forms.Form):
-    firstName = forms.CharField(max_length=40)
-    lastName = forms.CharField(max_length=40)
-    email = forms.EmailField()
+    firstName = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class': 'form-input'}))
+    lastName = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-input'}))
     message = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
