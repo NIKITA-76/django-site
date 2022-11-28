a5d33aad0a322c98683f560e4e73908b819aaba7
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-28 20:39:19 +0300

    add label foe email

--- DBLOG/blog_app/forms.py
+++ DBLOG/blog_app/forms.py
@@ -4,5 +4,5 @@ from django import forms
 class SendToMail(forms.Form):
     firstName = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class': 'form-input'}))
     lastName = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class': 'form-input'}))
-    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-input'}), label="Email (for feedback)")
     message = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
