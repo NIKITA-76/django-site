2777b1babdf5118ade3a4cb4861fc5863ef41412
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-28 20:32:39 +0300

    Processing of data from the website

--- DBLOG/blog_app/views.py
+++ DBLOG/blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
 from django.core.mail import send_mail
+from django.template.loader import render_to_string
 from .models import *
 
 from blog_app import forms
@@ -15,8 +16,19 @@ def contact(request):
     if request.method == 'POST':
         form = forms.SendToMail(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
-            send_mail('Contact subj', 'Hello world', '89522179992.ru@gmail.com', ['89522179992.ru@gmail.com'])
+            email = form.cleaned_data['email']
+            firstName = form.cleaned_data['firstName']
+            lastName = form.cleaned_data['lastName']
+            message = form.cleaned_data['message']
+            html_for_email = render_to_string("for_email.html", {
+                'email': email,
+                'firstName': firstName,
+                'lastName': lastName,
+                'message': message,
+
+            })
+
+            send_mail('Contact subj', 'Hello world', '89522179992.ru@gmail.com', ['nikita76twitch.ru@gmail.com'],html_message=html_for_email)
     else:
         form = forms.SendToMail()
     return render(request, 'contact_mail.html', {'form': form}, )
