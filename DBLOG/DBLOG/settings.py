bfa87e47befa52d5faa1fdbbf5a740eada2548b1
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-28 19:36:04 +0300

    Add email_backend

--- DBLOG/DBLOG/settings.py
+++ DBLOG/DBLOG/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+import django.core.mail.backends.base
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,6 +28,8 @@ SECRET_KEY = 'django-insecure-878h39!3$lotxqm+)f1#@7sdpkvub^anl@!$79mnvf#gxi=ji=
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
 # Application definition
