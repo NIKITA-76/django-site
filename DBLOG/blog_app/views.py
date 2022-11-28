3886a5a7df5db251f96c6dce107f41c9a6631cd2
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-28 19:36:18 +0300

    send_email()

--- DBLOG/blog_app/views.py
+++ DBLOG/blog_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from django.core.mail import send_mail
 from .models import *
 
 from blog_app import forms
@@ -15,6 +16,7 @@ def contact(request):
         form = forms.SendToMail(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            send_mail('Contact subj', 'Hello world', '89522179992.ru@gmail.com', ['89522179992.ru@gmail.com'])
     else:
         form = forms.SendToMail()
     return render(request, 'contact_mail.html', {'form': form}, )
