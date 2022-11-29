2eaf4ff5bc47d2eb507a9411b0fabc84c8ef3054
NIKITA-76 <89522179992.ru@gmail.com>
2022-11-30 02:43:37 +0300

    New way for sign page

--- DBLOG/blog_app/urls.py
+++ DBLOG/blog_app/urls.py
@@ -4,5 +4,6 @@ from blog_app import views
 urlpatterns = [
     path('', views.first_page),
     path('contact_to_mail/', views.contact, name="contact"),
+    path('sign_in/', views.sign_in, name="sign_in"),
 
 ]
