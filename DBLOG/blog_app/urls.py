376c96283930c798aa0dd88965ffccc1f052851d
NIKITA-76 <89522179992.ru@gmail.com>
2022-12-01 16:56:37 +0300

    Change url for update views.py

--- DBLOG/blog_app/urls.py
+++ DBLOG/blog_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from blog_app import views
 
 urlpatterns = [
-    path('', views.first_page),
+    path('', views.BlogHome.as_view(), name="home"),
     path('contact_to_mail/', views.contact, name="contact"),
     path('sign_in/', views.sign_in, name="sign_in"),
 
