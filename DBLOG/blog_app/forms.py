7866564fb235224ca964dee86ffea2dbe053a22d
NIKITA-76 <89522179992.ru@gmail.com>
2022-12-02 16:42:52 +0300

    New class for Auth

--- DBLOG/blog_app/forms.py
+++ DBLOG/blog_app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class SendToMail(forms.Form):
@@ -6,3 +7,8 @@ class SendToMail(forms.Form):
     lastName = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-input'}), label="Email (for feedback)")
     message = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}))
+
+
+class LoginUserForm(AuthenticationForm):
+    username = forms.CharField(label="Login", widget=forms.TextInput(attrs={'class': 'form-input'}))
+    password = forms.CharField(label="Password", widget=forms.TextInput(attrs={'class': 'form-input'}))
