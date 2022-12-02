adbfa362ae9020e7c979a5b305e0898d520c5b76
Tarasov Nikita <73834676+NIKITA-76@users.noreply.github.com>
2022-12-02 20:01:05 +0300

    Update views.py

--- DBLOG/blog_app/views.py
+++ DBLOG/blog_app/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView, AuthenticationForm
+from django.shortcuts import render, redirect
 from django.http import HttpResponseNotFound
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
-from django.views.generic import ListView
-
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from blog_app import models
 from blog_app import forms
+
+import blog_app
 
 
 class BlogHome(ListView):
@@ -14,13 +18,21 @@ class BlogHome(ListView):
     context_object_name = 'posts'
 
 
+class LoginUser(LoginView):
+    form_class = blog_app.forms.LoginUserForm
+    template_name = 'login.html'
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
 def sign_in(request):
     return render(request, 'sign_in.html')
 
 
 def contact(request):
     if request.method == 'POST':
-        form = forms.SendToMail(request.POST)
+        form = forms.SendToMail(data=request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
             firstName = form.cleaned_data['firstName']
@@ -36,6 +48,7 @@ def contact(request):
 
             send_mail('Contact subj', 'Hello world', '89522179992.ru@gmail.com', ['nikita76twitch.ru@gmail.com'],
                       html_message=html_for_email)
+            form = forms.SendToMail()
     else:
         form = forms.SendToMail()
     return render(request, 'contact_mail.html', {'form': form}, )
@@ -43,3 +56,8 @@ def contact(request):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound("Страница не найдена брат")
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
