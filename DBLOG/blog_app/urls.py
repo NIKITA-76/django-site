16f83c569dafc37ec117a65ce99102a700c1590b
Tarasov Nikita <73834676+NIKITA-76@users.noreply.github.com>
2022-12-04 19:48:00 +0300

    Update urls.py

--- DBLOG/blog_app/urls.py
+++ DBLOG/blog_app/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from blog_app import views
 
 urlpatterns = [
-    path('', views.BlogHome.as_view(), name="home"),
-    path('login/', views.LoginUser.as_view(), name="login"),
+    path('', views.first_page, name="home"),
+    #path('login/', views.LoginUser.as_view(), name="login"),
     path('logout/', views.logout_user, name="logout"),
     path('contact_to_mail/', views.contact, name="contact"),
     path('sign_in/', views.sign_in, name="sign_in"),
