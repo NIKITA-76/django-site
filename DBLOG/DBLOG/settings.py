8f1bfedbc6bfe5fdd26e16ccfb0fb16666db600c
NIKITA-76 <89522179992.ru@gmail.com>
2022-12-09 15:12:09 +0300

    Connect to BD

--- DBLOG/DBLOG/settings.py
+++ DBLOG/DBLOG/settings.py
@@ -85,11 +85,11 @@ SOCIAL_AUTH_JSONFIELD_ENABLED = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'NIKITA76',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': 'QPbiGkPEX1RCYPIJ8C8k',
+        'HOST': 'containers-us-west-121.railway.app',
+        'PORT': '7929',
     }
 }
 
