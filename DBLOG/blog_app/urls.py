db008584b775e75787fde1f4705533284e2d7383
NIKITA-76 <89522179992.ru@gmail.com>
2022-12-14 18:37:44 +0300

    New way for new page

--- DBLOG/blog_app/urls.py
+++ DBLOG/blog_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     #path('login/', views.LoginUser.as_view(), name="login"),
     path('logout/', views.logout_user, name="logout"),
     path('contact_to_mail/', views.contact, name="contact"),
+    path('project_about/', views.project_about, name="prj_abt"),
     path('sign_in/', views.sign_in, name="sign_in"),
     path('social-auth/', include('social_django.urls', namespace='social'))
 
